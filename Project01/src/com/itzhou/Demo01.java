de93f64b8323f194b2dc18f5a2085da5e8906341
Mr-xinan <44287951+Mr-xinan@users.noreply.github.com>
2019-04-09 12:04:06 +0800

    Update Demo01.java

--- Project01/src/com/itzhou/Demo01.java
+++ Project01/src/com/itzhou/Demo01.java
@@ -7,5 +7,6 @@ package com.itzhou;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("wwwwwwwwwwwwwwwwwwww");
+         System.out.println("wwwwwwwwwwwwwwwwwwww  github");
     }
 }
