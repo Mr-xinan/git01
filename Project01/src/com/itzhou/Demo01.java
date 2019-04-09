18aad898177a75d92daec44d8789c70d41346552
root <136043681@qq.com>
2019-04-09 12:08:37 +0800

    idea修改

--- Project01/src/com/itzhou/Demo01.java
+++ Project01/src/com/itzhou/Demo01.java
@@ -8,5 +8,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("wwwwwwwwwwwwwwwwwwww");
          System.out.println("wwwwwwwwwwwwwwwwwwww  github");
+         System.out.println("wwwwwwwwwwwwwwwwwwww  idea");
     }
 }
